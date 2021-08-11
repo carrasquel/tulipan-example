9f270cf0a2d6503c6c0ae51c63753364c723e518
Nelson Carrasquel <carrsquel@outlook.com>
2021-08-11 13:09:08 -0400

    vendor cfixed

--- src/vendor.js
+++ src/vendor.js
@@ -3,5 +3,5 @@ import css from "./static/css/bootstrap.min.css";
 import './static/js/jquery.min.exec.js';
 import './static/js/bootstrap.min.exec.js';
 import './static/js/bootbox.min.exec.js';
-import './static/js/tulipan.min.exec.js';
+import './static/js/tulipan.exec.js';
 import './script/dialogPlugin.exec.js';
