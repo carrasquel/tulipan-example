f1183465683fd21fac93960df330ee82cfd01629
Nelson Carrasquel <carrsquel@outlook.com>
2021-08-11 13:06:45 -0400

    files into min

--- src/vendor.js
+++ src/vendor.js
@@ -1,7 +1,7 @@
-import css from "./static/css/bootstrap.css";
+import css from "./static/css/bootstrap.min.css";
 
 import './static/js/jquery.min.exec.js';
-import './static/js/bootstrap.exec.js';
+import './static/js/bootstrap.min.exec.js';
 import './static/js/bootbox.min.exec.js';
-import './static/js/tulipan.exec.js';
+import './static/js/tulipan.min.exec.js';
 import './script/dialogPlugin.exec.js';
